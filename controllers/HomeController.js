35772d215d9bb7c37d012a46921e4c7b0823c98a
Sahos Mia <sahosmia.webdev@gmail.com>
2024-11-12 00:00:14 +0600

    Authentication Add

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,8 +1,8 @@
 const zeroPoint = (req, res) => {
   console.log("home route");
-  // res.send("hello");
-  const data = "sahos Data";
-  res.render("home", { data: false });
+  res.send("hello");
+  // const data = "sahos Data";
+  // res.render("home", { data: false });
 };
 
 module.exports = { zeroPoint };
